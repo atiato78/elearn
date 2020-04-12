8f43db078dbb30b6bf5752479a358be2e091ad89
Omar <omar@atiato.local>
2020-04-12 15:07:59 +0300

    firstcommit-1-voice

---
+++ src/main/java/io/fabric8/quickstarts/camel/voice/VoiceSession.java
@@ -0,0 +1,33 @@
+package io.fabric8.quickstarts.camel.voice;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class VoiceSession {
+
+
+    private String voiceid; 
+    private String creation_date;
+
+    public String getVoiceid() {
+        return voiceid;
+    }
+
+    public void setVoiceid(String voiceid) {
+        this.voiceid = voiceid;
+    }
+
+ public String getCreation_date() {
+        return creation_date;
+    }
+
+    public void setCreation_date(String creation_date) {
+        this.creation_date = creation_date;
+    }
+
+
+
+  
+
+
+}
